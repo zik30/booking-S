44423bc485251a2f93f9cd9643769e1d8a1946ec
zik30 <ainazik.ishenbek@gmail.com>
2025-11-25 14:32:04 +0600

    f

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,9 +1,15 @@
 import type { FC } from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export const Layout: FC = () => {
   return (
     <div>
+      <header>
+        <Link to={"open-space"}>open</Link>
+        <Link to={"silent-space"}>silent</Link>
+        <Link to={"period"}>multi</Link>
+        <Link to={"permanent"}>Perm</Link>
+      </header>
       <main>
         <Outlet />
       </main>
